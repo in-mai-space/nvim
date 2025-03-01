563e2729061e31373a5e101f3b16a8557937617d
mai <thisismainguyen@gmail.com>
2025-02-28 23:40:14 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.conceallevel = 0
+vim.opt.scrolloff = 10
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab = true
+vim.opt.smartindent=true
